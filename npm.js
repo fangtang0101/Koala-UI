7a6bc5bda8cb49d9a686d339267ed8ddad91e2dd
ZweiZhao <ZweiZhao@foxmail.com>
2018-01-02 16:30:31 +0800

    首次发布 npm

---
+++ npm.js
@@ -0,0 +1,9 @@
+import zHeader from './src/components/header.vue'
+import zContent from './src/components/content.vue'
+import zIconButton from './src/components/icon-button.vue'
+
+module.exports = {
+  zHeader,
+  zContent,
+  zIconButton
+}
